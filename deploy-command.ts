07362df9f9757fdc842f6fb84ab657b9d677a02f
Wonbin Moon <wonbin.moon@gmail.com>
2021-10-07 11:56:52 -0400

    ksm slash command

--- deploy-command.ts
+++ deploy-command.ts
@@ -7,7 +7,8 @@ const commands = [
     new SlashCommandBuilder().setName('fbike').setDescription('fuck bikes'),
     new SlashCommandBuilder().setName('amc').setDescription('How\'s AMC doing today?'),
     new SlashCommandBuilder().setName('gme').setDescription('How\'s GME doing today?'),
-    new SlashCommandBuilder().setName('btc').setDescription('How\'s Bitgoin doing today?')
+    new SlashCommandBuilder().setName('btc').setDescription('How\'s Bitgoin doing today?'),
+    new SlashCommandBuilder().setName('ksm').setDescription('Do you know Kim Sungmo?')
 ]
     .map(command => command.toJSON());
 
