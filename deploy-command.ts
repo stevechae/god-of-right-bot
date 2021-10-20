f52ce1db344f4244233e03486400b4cffe609d96
gg <gg.sj.yoon@gmail.com>
2021-10-20 17:36:28 -0400

    quiz boss

--- deploy-command.ts
+++ deploy-command.ts
@@ -8,17 +8,24 @@ const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
     new SlashCommandBuilder().setName('fbike').setDescription('fuck bikes'),
-    new SlashCommandBuilder().setName('amc').setDescription('How\'s AMC doing today?'),
-    new SlashCommandBuilder().setName('gme').setDescription('How\'s GME doing today?'),
     new SlashCommandBuilder().setName('btc').setDescription('How\'s Bitgoin doing today?'),
     new SlashCommandBuilder().setName('ksm').setDescription('Do you know Kim Sungmo?'),
     new SlashCommandBuilder().setName('emperor').setDescription('Inspiring quotes from the Emperor'),
     new SlashCommandBuilder().setName('weather').setDescription('How\'s weather now?')
-    .addStringOption((option: SlashCommandStringOption) => option.setName('postal').setDescription('First 3 chars of postal code').setRequired(true)),
+        .addStringOption((option: SlashCommandStringOption) => option.setName('postal').setDescription('First 3 chars of postal code').setRequired(true)),
     new SlashCommandBuilder().setName('stock').setDescription('Any stock quote')
         .addStringOption((option: SlashCommandStringOption) => option.setName('symbol').setDescription('Stock symbol').setRequired(true)),
     new SlashCommandBuilder().setName('food').setDescription('What do I eat?')
         .addStringOption((option: SlashCommandStringOption) => option.setName('postal').setDescription('Your postal code').setRequired(true)),
+    new SlashCommandBuilder().setName('quiz').setDescription('Riddle me this.')
+        .addStringOption(
+            (option: SlashCommandStringOption) => option.setName('category')
+            .setDescription('Linux, DevOps, Docker, or BASH')
+            .setRequired(true)
+            .addChoice('Linux', 'Linux')
+            .addChoice('DevOps', 'DevOps')
+            .addChoice('Docker', 'Docker')
+            .addChoice('BASH', 'BASH')),
 ]
     .map(command => command.toJSON());
 
