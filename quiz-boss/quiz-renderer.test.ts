256d2ab46a671d2967b62a719cb8cd736b8ebbf9
SJ <gg.sj.yoon@gmail.com>
2021-11-02 13:15:07 -0400

    added a test case

--- quiz-boss/quiz-renderer.test.ts
+++ quiz-boss/quiz-renderer.test.ts
@@ -1,3 +1,5 @@
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import { CleanQuiz, DirtyQuiz } from "./quiz";
 import { QuizRenderer } from "./quiz-renderer"
 
 describe("QuizRenderer", () => {
@@ -16,6 +18,47 @@ describe("QuizRenderer", () => {
 
         it("should throw a new Error if apiKey is missing 2.", () => {
             expect(() => { new QuizRenderer("test_api_url", "") }).toThrowError(new Error("API Key is missing."));
+        })
+    })
+
+    describe("fetchQuizData(category, limit, fetchDirtyQuiz)", () => {
+        it("should return Promise<CleanQuiz>", () => {
+            const dirtyQuiz = {
+                id: 1,
+                question: "?",
+                description: null,
+                answers: { answer_a: "X" },
+                multiple_correct_answers: "false",
+                correct_answers: { answer_a_correct: "false" },
+                correct_answer: "answer_a",
+                explanation: null,
+                tip: null,
+                category: "Linux",
+                difficulty: "EZ",
+            };
+            const quizApiUrl = "test_api_url";
+            const axiosResponse: AxiosResponse<DirtyQuiz[]> = {
+                data: [dirtyQuiz],
+                status: 200,
+                statusText: "OK",
+                headers: {
+                    "test_key": "test_value"
+                },
+                config: {}
+              }
+            const fetchQuizData = (quizApiUrl: string, config?: AxiosRequestConfig<DirtyQuiz[]>) => Promise.resolve(axiosResponse);
+            const result: Promise<CleanQuiz> = new QuizRenderer(quizApiUrl, "test_api_key").fetchQuizData("Linux", 1, fetchQuizData);
+
+            result.then((cleanQuiz: CleanQuiz) => {
+                expect(cleanQuiz.question).toBe("?");
+                expect(cleanQuiz.description).toBe(null);
+                expect(cleanQuiz.choices).toStrictEqual([ { name: "A", value: "X" } ]);
+                expect(cleanQuiz.answers).toStrictEqual([]);
+                expect(cleanQuiz.explanation).toBe(null);
+                expect(cleanQuiz.tip).toBe(null);
+                expect(cleanQuiz.category).toBe("Linux");
+                expect(cleanQuiz.difficulty).toBe("EZ");
+            });
         })
     })
 })
